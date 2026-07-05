ed364d417c1eccdefac346dae54f4c350728baa4
Saravanan21-dev <Saravanan.learning21@gmail.com>
2026-07-05 21:16:59 +0530

    test file added

---
+++ test_app.py
@@ -0,0 +1,18 @@
+import pytest
+from app import app   # import your Flask app
+
+@pytest.fixture
+def client():
+    # Flask provides a test client for simulating HTTP requests
+    with app.test_client() as client:
+        yield client
+
+def test_home_route(client):
+    # Send a GET request to "/"
+    response = client.get("/")
+    
+    # Check status code
+    assert response.status_code == 200
+    
+    # Check response body
+    assert response.data.decode("utf-8") == "Hello from Azure WebApp via GitHub Actions CI/CD!"
