18123614c2717cabf6b52da7371e2faebf3f3ab7
Saravanan21-dev <saravanan.learning21@gmail.com>
2026-07-05 21:28:33 +0530

    Fix Terraform: replace deprecated azurerm_app_service with azurerm_linux_web_app

--- main.tf
+++ main.tf
@@ -7,23 +7,23 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 }
 
-resource "azurerm_app_service_plan" "asp" {
+resource "azurerm_service_plan" "asp" {
   name                = "asp-github-cicd"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
-resource "azurerm_app_service" "app" {
+resource "azurerm_linux_web_app" "app" {
   name                = "my-azure-webapp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
-    python_version = "3.10"
+    application_stack {
+      python_version = "3.10"
+    }
   }
 }
